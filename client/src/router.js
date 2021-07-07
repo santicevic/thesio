0d33b038c6ca60a46117c717179446ab13bc7792
Sergije <sergijeanticevic@gmail.com>
2021-07-07 12:47:08 +0200

    Set up client

---
+++ client/src/router.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import LoginScreen from './screens/LoginScreen';
+
+const Router = () => {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route path="/">
+          <LoginScreen />
+        </Route>
+      </Switch>
+    </BrowserRouter>
+  );
+};
+
+export default Router;
